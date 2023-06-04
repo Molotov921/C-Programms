c3ec3e0b012c5bbcbd78cbfe62470c43240ef828
Neel Kalariya <kalarianeel249@gmail.com>
2023-06-04 21:24:27 +0530

    Add files via upload

---
+++ R&W_Flutter_Dev - C-lang/Day-8/operator.c
@@ -0,0 +1,13 @@
+/*wap to perform ternary operator*/
+#include<stdio.h>
+void main()
+{
+	int a,b;
+
+	printf("Enter the value of a = ");
+	scanf("%d",&a);
+	printf("Enter the value of b = ");
+	scanf("%d",&b);
+	
+	a>b?printf("a is bigger..."):printf("b is bigger...");
+}
