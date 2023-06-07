daf4700a60ea687d91250ade866d37097ea5e8b4
Neel Kalariya <kalarianeel249@gmail.com>
2023-06-07 13:36:14 +0530

    Add files via upload

---
+++ R&W_Flutter_Dev - C-lang/Day-11/do.c
@@ -0,0 +1,17 @@
+/*wap to print number using do-while loop*/
+#include<stdio.h>
+void main()
+{
+	int a=1,i=5;
+	do
+	{
+		printf("%d\n",a);
+		a++;
+	}while(a<=10);
+
+	while(i<=10)
+	{
+		printf("%d\n",i);
+		i++;
+	}
+}
